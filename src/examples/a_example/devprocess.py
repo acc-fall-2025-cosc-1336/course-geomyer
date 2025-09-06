4852d4f242b3712cf20c2bb6460a13e6d3c4fa04
geomyer <geoffrey.myers@g.austincc.edu>
2025-09-06 17:06:43 +0000

    Define Functions for multiplying numbers.

--- src/examples/a_example/devprocess.py
+++ src/examples/a_example/devprocess.py
@@ -1,3 +1,15 @@
-def add_numbers(val1, val2):
-     '''Type the correct code to add two numbers after the return statement.'''
-     return val1 + val2
+def multiply_numbers(val1, val2):
+    '''Returns the product of two numbers.'''
+    return val1 * val2
+
+def multiply_numbers(val1, val2):
+    '''Returns the product of two numbers.'''
+    return val1 * val2
+def multiply_numbers(val1, val2):
+    '''Returns the product of two numbers.'''
+    return val1 * val2
+
+print(multiply_numbers(7, 7))
+print()
+print(multiply_numbers(5, 5))
+print() 
--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,5 +1,10 @@
-import devprocess
+from devprocess import add_numbers, multiply_numbers
 
-result = devprocess.add_numbers(5, 5)
 
-print(result)
+print('Hello Word')
+print('What is this I am seeing?')
+print('This is really cool!')
+def Hello_World():
+    print("Hello, World!")
+
+Hello_World()
