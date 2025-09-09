6e309c8168a8982866444591c81cff08ffb0a7ed
geomyer <geoffrey.myers@g.austincc.edu>
2025-09-09 23:57:26 +0000

    Homework 2 Completed

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,11 +1,3 @@
 def multiply_numbers(val1, val2):
     '''Returns the product of two numbers.'''
     return val1 * val2
-def multiply_numbers(val1, val2):
-    '''Returns the product of two numbers.'''
-    return val1 * val2
-
-print(multiply_numbers(7, 7))
-print()
-print(multiply_numbers(5, 5))
-print() 
