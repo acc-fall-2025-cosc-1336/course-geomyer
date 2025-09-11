5416a535acd7413009e4c760aa73f64a53400f46
geomyer <geoffrey.myers@g.austincc.edu>
2025-09-11 19:48:34 +0000

    Home Assignment 2

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -3,5 +3,17 @@ from devprocess import multiply_numbers
 
 def main():
 
-if __name__ == "__main__":
-    main()
+    subtotal = 100
+    sales_tax_rate = 0.825
+    tip_percentage = 0.15
+
+    sales_tax = multiply_numbers(subtotal, sales_tax_rate)
+    tip = multiply_numbers(subtotal, tip_percentage)
+    total = subtotal + sales_tax + tip
+
+    print(f'Subtotal: ${subtotal:.2f}')
+    print(f'Sales Tax: ${sales_tax:.2f}')
+    print(f'Tip: ${tip:.2f}')
+    print(f'Total: ${total:.2f}')   
+
+ 
